e6c562b6fc3052493f2ff24f296a4a3178ea7e7c
yishaling <646856191@qq.com>
2019-11-26 23:04:39 +0800

    init study

--- src/main/java/bilibili_huangjun/Java19NoNameInnerClass.java
+++ src/main/java/bilibili_huangjun/Java19NoNameInnerClass.java
@@ -1,12 +1,17 @@
 package bilibili_huangjun;
 
-public class Java19NoNameInnerClass {
+public class Java19NoNameInnerClass<T extends Java19NoNameInnerClass> {
+    int i=1;
     public static void main(String[] args) {
+            new Java19NoNameInnerClass().method();
+
+    }
+    void method(){
         new Interface(){
 
             @Override
             public void print() {
-
+                System.out.println(i);
             }
         }.print();
     }
