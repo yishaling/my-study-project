75346951b1d33f97df9283b481e3a798ae2d45d4
yishaling <646856191@qq.com>
2020-01-13 21:46:41 +0800

    http req

--- dubbo-consumer/src/main/java/com/mashibing/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/mashibing/DubboConsumerApplication.java
@@ -14,26 +14,9 @@ import java.util.concurrent.Semaphore;
 @EnableDubboConfiguration
 @SpringBootApplication
 public class DubboConsumerApplication {
-  static   ExecutorService service= Executors.newFixedThreadPool(64);
-  static Semaphore semaphore=new Semaphore(256);
     public static void main(String[] args) throws InterruptedException {
-        for(;;){
-            semaphore.acquire();
-            service.submit(()->{
 
-                try {
-                    JSONObject jsonObject = HttpClientUtil.sendGet("http://athena-dispatch.maoniuchuxing.com/ws/v2.8/statistics/get-distributor-cost-all-count-info?startDate=1970-01-01&endDate=2099-01-01", new HashMap());
-                    System.out.println(jsonObject.toJSONString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }finally {
-                    semaphore.release();
-                }
-            });
-
-        }
-
-//        SpringApplication.run(DubboConsumerApplication.class);
+        SpringApplication.run(DubboConsumerApplication.class);
     }
 
 }
