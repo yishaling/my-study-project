d433de6b092bc3bd969ee54ab17a3e3392dbba98
yishaling <646856191@qq.com>
2020-02-17 02:20:42 +0800

    原生动态代理

--- src/main/java/design_pattern/RunProxyDemo.java
+++ src/main/java/design_pattern/RunProxyDemo.java
@@ -20,6 +20,7 @@ public class RunProxyDemo {
     public static void main(String[] args) {
         Station station=new Station();
         //每次 stationProxy 执行任何方法的时候 都会进入 invoke （除了native 本地方法除外 如 ：hashCode）
+        //JDBC 线程池就是 当getConnection 的时候 从List 里面remove，然后close的时候再把那个Connection 放回去
         IStation stationProxy = (IStation)Proxy.newProxyInstance(RunProxyDemo.class.getClassLoader(), new Class[]{IStation.class}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
