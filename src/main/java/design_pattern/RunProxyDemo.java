351cf901f5ac82e3bb6bc70c4d9f483c9da4e064
yishaling <646856191@qq.com>
2020-02-16 22:12:05 +0800

    Java Proxy

---
+++ src/main/java/design_pattern/RunProxyDemo.java
@@ -0,0 +1,36 @@
+package design_pattern;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface IStation{
+    public void sale();
+}
+class Station implements IStation{
+    public void sale(){
+        System.out.println("Station sale");
+    }
+    public String toString(){
+        System.out.println("--------------");
+        return "";
+    }
+}
+public class RunProxyDemo {
+    public static void main(String[] args) {
+        Station station=new Station();
+        //每次 stationProxy 执行任何方法的时候 都会进入 invoke （除了native 本地方法除外 如 ：hashCode）
+        IStation stationProxy = (IStation)Proxy.newProxyInstance(RunProxyDemo.class.getClassLoader(), new Class[]{IStation.class}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("被代理了");
+                method.invoke(station,null);
+                System.out.println("被调用方法："+method.getName());
+                return null;
+            }
+        });
+        stationProxy.sale(); // invoke 里面的Method 就会是同一个
+        stationProxy.toString();
+
+    }
+}
